53c69e4f56bb7ea231c3195afe6e044b35f0aa57
Horacio Torres <horaciotg@hotmail.com>
2020-03-24 22:45:01 -0600

    adding readme.txt

--- dockertraining_horacio_torres/Controllers/HomeController.cs
+++ dockertraining_horacio_torres/Controllers/HomeController.cs
@@ -4,14 +4,27 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 using dockertraining_horacio_torres.Models;
 
 namespace dockertraining_horacio_torres.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.EnvirionmentVariable = _configuration.GetSection("AppSettings").GetSection("storename").Value;
             return View();
         }
 
---
+++ dockertraining_horacio_torres/Readme.txt
@@ -0,0 +1,8 @@
+﻿Create docker image
+docker build -t myimage .
+
+Create a Docker Container using the image. Assign "Site1" as container name and port "8085" to run the container.
+docker run --name  Site1 -p  8085:80 myimage
+
+Create a Docker Container using the image. Assign "Site2" as container name, port "8086" and "storename" as "Plano".
+docker run --name  Site2 -p  8086:80 myimage -e "AppSettings:storename=Plano"
